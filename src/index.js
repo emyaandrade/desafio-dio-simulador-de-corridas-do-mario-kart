b6b0da43b66c0596c45fd5293a985e160dea63d3
Emilly Andrade <emilly.andrade.digital@gmail.com>
2025-07-22 15:32:26 -0300

    comecei a estruturar o index.js

--- src/index.js
+++ src/index.js
@@ -45,3 +45,46 @@ const player6 = {
     PODER: 5,
     PONTOS: 0
 };
+
+//função assíncrona para simular a rolagem de dados
+async function rollDice(){
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+async function getRandomBlock(){
+    let random = Math.random()
+    let result 
+
+    switch (true) {
+        case random < 0.33:
+            result = "RETA";
+            break;
+        case random < 0.66:
+            result = "CURVA";
+            break;
+        default:
+            result = "CONFRONTO";
+            break;
+    }
+
+    return result
+}
+
+async function playRaceEngine(character1, character2){
+    for(let round = 1; round <= 5; round++){
+        console.log(`🏁 Rodada ${round}`);
+
+        // sortear bloco
+        let block = await getRandomBlock()
+        console.log(`Bloco: ${block}`)
+    }
+}
+
+//função principal (auto invoke) para chamar as outras funções
+(async function main(){
+    console.log(
+        `🏁🚨 Corrida entre: ${player1.NOME} e ${player2.NOME} começando...\n`
+    );
+
+    await playRaceEngine(player1, player2);
+})()
